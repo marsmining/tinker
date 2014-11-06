4bfa3183d7c9a0a9a118c8575b0dd3963d58aa71
marsmining <marsmining@gmail.com>
2014-11-06 20:18:31 +0100

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,19 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var path = require("path");
 
+var lessGlob = "less/*.less";
+
 gulp.task("less", function () {
-    gulp.src("./less/*.less")
+    gulp.src(lessGlob)
         .pipe(less({
             paths: [ path.join(__dirname, "less", "includes") ]
         }))
         .pipe(gulp.dest("./public/css"));
+});
+
+gulp.task("watch", function () {
+    var watcher = gulp.watch(lessGlob, ["less"]);
+    watcher.on("change", function(event) {
+        console.log("File " + event.path + " was " + event.type + ", running tasks...");
+    });
 });
